fec1ed8961104d5ae501e48750de096f2ada8d29
CarlaLourenco <cristinasilvacarla31@gmail.com>
2023-12-06 21:51:57 -0300

    Configuração template engine

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express"
+import { engine } from "express-handlebars";
 import path from "path"
-
+import moment from "moment"
 
 const app = express()
 
@@ -11,6 +12,22 @@ app.use(
   })
 );
 app.use(express.json());
+app.engine(
+  "handlebars",
+  engine({
+    helpers: {
+      formatDate(date) {
+        return moment(date).format("DD/MM/YYYY");
+      },
+      eq(a, b, options) {
+        return a === b ? a : b;
+      },
+    },
+    defaultLayout: "main",
+  })
+);
+app.set("view engine", "handlebars");
+app.set("views", "./views");
 
 app.use(express.static (path.join(path.resolve(),"public")));
 app.locals.publicPath="/public"
